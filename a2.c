877809fac7fc0077b860c66aa1f984ab463f04d8
adwaithhs <adwaithhs@gmail.com>
2023-09-20 15:16:13 +0530

    commit2

--- a2.c
+++ a2.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <inttypes.h>
+#include <dirent.h> 
+#include <mpi.h>
 
 uint64_t *read_input(char *file) {
     FILE *fs = fopen(file, "r");
@@ -13,7 +15,42 @@ uint64_t *read_input(char *file) {
     return data;
 }
 
-int main(int argc, char const *argv[]) {
-    /* code */
+int main(void) {
+  
+  return(0);
+}
+
+int main(int argc, char** argv) {
+    int rank, size, message_Item;
+
+    char input_file[] = "input/";
+    char output_file[] = "output/";
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    DIR *d;
+    struct dirent *dir;
+    d = opendir(input_file);
+    if (d) {
+        while ((dir = readdir(d)) != NULL) {
+            if (dir->d_type == DT_REG) {
+                printf("%s\n", dir->d_name);
+            }
+        }
+        closedir(d);
+    }
+    if(rank == 0){
+        message_Item = 42;
+        MPI_Send(&message_Item, 1, MPI_INT, 1, 1, MPI_COMM_WORLD);
+        printf("Message Sent: %d\n", message_Item);
+    }
+
+    else if(rank == 1){
+        MPI_Recv(&message_Item, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        printf("Message Received: %d\n", message_Item);
+    }
+
+    MPI_Finalize();
     return 0;
 }
---
+++ test.c
@@ -0,0 +1,16 @@
+#include <dirent.h> 
+#include <stdio.h> 
+
+int main(void) {
+  DIR *d;
+  struct dirent *dir;
+  d = opendir("input");
+  if (d) {
+    while ((dir = readdir(d)) != NULL) {
+        if (dir->d_type == DT_REG)
+            printf("%s\n", dir->d_name);
+    }
+    closedir(d);
+  }
+  return(0);
+}
