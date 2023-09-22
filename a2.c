291a9f899249ac5b836212e530d2675145a93471
adwaithhs <adwaithhs@gmail.com>
2023-09-22 15:17:21 +0530

    commit4

--- a2.c
+++ a2.c
@@ -1,41 +1,68 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <inttypes.h>
 #include <dirent.h> 
 #include <mpi.h>
 
 #define DT_REG 8
 
-uint64_t *read_input(char *file) {
+int comp(const void *elem1, const void *elem2) 
+{
+    uint32_t f = *((uint32_t*)elem1);
+    uint32_t s = *((uint32_t*)elem2);
+    if (f > s) return  1;
+    if (f < s) return -1;
+    return 0;
+}
+
+void *read_input(char *file, uint64_t ** p_data, uint64_t *p_n) {
     FILE *fs = fopen(file, "r");
-    uint64_t n;
-    fscanf(fs, "%" SCNu64, &n);
-    uint64_t* data = malloc(n*sizeof(*data));
-    for (uint64_t i = 0; i < n; i++)
+    fscanf(fs, "%" SCNu64, p_n);
+    *p_data = malloc(*p_n*sizeof(uint64_t));
+    for (uint64_t i = 0; i < *p_n; i++)
     {
-        fscanf(fs, "%" SCNu64, data + i);
+        fscanf(fs, "%" SCNu64, *p_data + i);
     }
-    return data;
 }
 
 int main(int argc, char** argv) {
-    int rank, size, message_Item;
+    int rank, p, message_Item;
 
-    char input_file[] = "input/";
-    char output_file[] = "output/";
+    char input_path[] = "input/";
+    char output_path[] = "output/";
 
     MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_size(MPI_COMM_WORLD, &p);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    if (rank == 0) {
-        DIR *d;
-        struct dirent *dir;
-        d = opendir(input_file);
-        while ((dir = readdir(d)) != NULL) {
-            if (dir->d_type == DT_REG) {
-                //file = dir->d_name;
+    DIR *d;
+    struct dirent *dir;
+    int i = 0;
+    char *file = NULL;
+    d = opendir(input_path);
+    while ((dir = readdir(d)) != NULL) {
+        if (dir->d_type == DT_REG) {
+            if (i == rank) {
+               file = dir->d_name;
+               break;
             }
+            i++;
         }
-        closedir(d);
+    }
+    closedir(d);
+    uint64_t* data;
+    uint64_t n;
+    read_input(file, &data, &n);
+
+    qsort(data, n, sizeof(uint64_t), comp);
+    uint64_t* ps = malloc(p*sizeof(uint64_t));
+    uint64_t* pseudo_splitters;
+    if (rank == 0) {
+        pseudo_splitters = malloc(p*p*sizeof(uint64_t));
+    }
+    MPI_Gather(ps, p, MPI_INT64_T, pseudo_splitters, p, MPI_INT64_T, 0, MPI_COMM_WORLD);
+
+    if (rank == 0) {
+        qsort(pseudo_splitters, p*p, sizeof(uint64_t), comp);
     }
 
     if(rank == 0){
--- test.c
+++ test.c
@@ -1,6 +1,18 @@
 #include <dirent.h> 
+#include <stdlib.h>
 #include <stdio.h> 
 #include <string.h>
+#include <inttypes.h>
+
+void *read_input(char *file, uint64_t ** p_data, uint64_t *p_n) {
+    FILE *fs = fopen(file, "r");
+    fscanf(fs, "%" SCNu64, p_n);
+    *p_data = malloc(*p_n*sizeof(uint64_t));
+    for (uint64_t i = 0; i < *p_n; i++)
+    {
+        fscanf(fs, "%" SCNu64, *p_data + i);
+    }
+}
 
 int main(void) {
   DIR *d;
@@ -14,5 +26,13 @@ int main(void) {
     closedir(d);
   }
   printf("%lu\n", strlen("test"));
+  uint64_t *data;
+  uint64_t n;
+  read_input("input/in_0.txt", &data, &n);
+  for (uint64_t i = 0; i < n; i++)
+  {
+    printf("%" PRIu64 "\n", data[i]);
+  }
+  
   return(0);
 }
